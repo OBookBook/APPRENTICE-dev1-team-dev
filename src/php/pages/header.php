d92f4467b5736c68ca342a0155abae96a42f85c4
SayuriOkuyama <mosyaneko77@gmail.com>
2023-11-27 15:50:54 +0900

    img ディレクトリを作成し、ロゴ画像保存。 style.css に reset.css の記述を含め、共通で使うcssを変数で定義。 header.php を作成。

---
+++ src/php/pages/header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>デイリポ</title>
+  <link rel="stylesheet" href="../../css/style.css">
+</head>
+
+<body>
+  <header>
+    <h1><img src="../../img/logo.png" alt="デイリポ"></h1>
+    <nav>
+      <ul>
+        <li>アカウント設定</li>
+        <li>ログアウト</li>
+      </ul>
+    </nav>
+    <div>
+      <img src="" alt="ユーザーアイコン">
+      <div><?php echo "UserName" ?></div>
+    </div>
+  </header>
+</body>
+
+</html>
