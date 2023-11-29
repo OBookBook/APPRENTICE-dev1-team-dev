bd15ac2925bc304ed518e947052b3bc43faf4301
SayuriOkuyama <mosyaneko77@gmail.com>
2023-11-29 22:25:11 +0900

    タスク登録・削除・表示機能追加

---
+++ src/php/functions/TaskControl.php
@@ -0,0 +1,72 @@
+<?php
+
+require_once(__DIR__ . '/../functions/connectSql.php');
+
+function getInputTask($userId, $date)
+{
+  $inputTask = $_POST['input_task'];
+
+  $dbh = connectSql();
+
+  $query = <<<EOT
+  INSERT INTO tasks
+    (user_id,
+    task_name,
+    execution_date,
+    completion_status,
+    display_order)
+  VALUES
+    (:userId,
+    :task,
+    :date,
+    0,
+    (SELECT (MAX(display_order) + 1)
+      FROM tasks as t
+    WHERE t.user_id = :userId))
+EOT;
+
+  $stmt = $dbh->prepare($query);
+  $stmt->bindParam(':userId', $userId, PDO::PARAM_INT);
+  $stmt->bindParam(':date', $date, PDO::PARAM_STR);
+  $stmt->bindParam(':task', $inputTask, PDO::PARAM_STR);
+  $stmt->execute();
+
+  $tasks = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+  $dbh = null;
+
+  return $tasks;
+
+  echo $inputTask;
+}
+
+
+function deleteTask()
+{
+  $taskId = $_POST['delete_task_id'];
+
+  $dbh = connectSql();
+
+  $query = <<<EOT
+      DELETE FROM tasks
+      WHERE task_id = :taskId
+  EOT;
+
+  $stmt = $dbh->prepare($query);
+  $stmt->bindParam(':taskId', $taskId, PDO::PARAM_INT);
+  $stmt->execute();
+
+  $dbh = null;
+}
+
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  if (isset($_POST['form_id'])) {
+    if ($_POST['form_id'] === 'delete_task') {
+      deleteTask();
+    } elseif ($_POST['form_id'] === 'input_task') {
+      getInputTask(1, '2023-11-20');
+    }
+  }
+}
+header("Location: ../pages/task-management.php");
--- src/php/pages/index.php
+++ src/php/pages/index.php
@@ -1,11 +1,30 @@
 <?php
 require_once(__DIR__ . '/header.php');
 require_once(__DIR__ . '/footer.php');
+require_once(__DIR__ . '/../functions/connectSql.php');
+require_once(__DIR__ . '/../functions/ShowTasks.php');
 
 getHeader();
 ?>
 <main>
-    <h1>デイリポ！</h1>
+  <section id="task-management">
+    <h2><span>〇月〇日のタスク一覧</span></h2>
+    <ul class="task-lists">
+      <?php
+      // 仮に user_id が 1 のユーザー、日付を2023-11-20として実装
+      $userId = 1;
+      $date = '2023-11-20';
+      showTasks($userId, $date);
+      ?>
+      <li class="task-list">
+        <form class="add_task_form" action="../functions/TaskControl.php" method="post">
+          <input type="hidden" name="form_id" value="input_task">
+          <input class="input_task" type="text" name="input_task">
+          <button class="add_task_btn" type="submit">＋</button>
+        </form>
+      </li>
+    </ul>
+  </section>
 </main>
 <?php
 getFooter();
